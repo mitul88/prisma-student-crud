b4675f8852e44c2d4e34039085efa00419ba815d
mitul88 <shahed.rahman88@gmail.com>
2023-12-21 21:28:45 +0600

    multiple entry api done

--- src/app/api/student/create/route.js
+++ src/app/api/student/create/route.js
@@ -4,12 +4,25 @@ import { NextResponse } from 'next/server'
 export async function POST(req,res) {
     const reqBody=await req.json();
 
+    const isArray = Array.isArray(reqBody)
+
     try {
         const prisma = new PrismaClient();
-        let result = await prisma.user.create({
-            data: reqBody
-        })
-        return NextResponse.json({message:"success! Student created!!", data: result}, {status: 201})
+        
+        if(isArray) {
+            let result = await prisma.user.createMany({
+                data: reqBody
+            })
+            return NextResponse.json({message:`Success! total ${result['count']} entry created!`, data: result}, {status: 201})            
+        }
+        
+        if(!isArray) {
+            let result = await prisma.user.create({
+                data: reqBody
+            })
+            return NextResponse.json({message:"success! Student created!!", data: result}, {status: 201})
+        }
+
     } catch(error) {
         return NextResponse.json({message:"Something is not right!!"}, {status:500})       
     }
