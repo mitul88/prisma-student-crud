a5923318661a7bf8e4a8deb4a927025538b8e08d
mitul88 <shahed.rahman88@gmail.com>
2023-12-24 13:26:57 +0600

    all api done, readme updated

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -18,6 +18,33 @@ export async function GET(req,res) {
     }
 }
 
+export async function POST(req,res) {
+    const reqBody=await req.json();
+
+    const isArray = Array.isArray(reqBody)
+
+    try {
+        const prisma = new PrismaClient();
+        
+        if(isArray) {
+            let result = await prisma.user.createMany({
+                data: reqBody
+            })
+            return NextResponse.json({message:`Success! total ${result['count']} entry created!`, data: result}, {status: 201})            
+        }
+        
+        if(!isArray) {
+            let result = await prisma.user.create({
+                data: reqBody
+            })
+            return NextResponse.json({message:"success! Student created!!", data: result}, {status: 201})
+        }
+
+    } catch(error) {
+        return NextResponse.json({message:"Something is not right!!"}, {status:500})       
+    }
+}
+
 export async function PUT(req,res) {
     const reqBody=await req.json();
     const {searchParams} = new URL(req.url)
